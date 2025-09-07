7a98274cae7723caa449cfd4303bd3030f76faf2
Bhargav-sunil <bhargavsunil2166@gmail.com>
2025-09-07 19:49:29 +0530

    Update Login.jsx

--- Client/src/components/Login/Login.jsx
+++ Client/src/components/Login/Login.jsx
@@ -16,7 +16,7 @@ const Login = ({ setCurrentView, onLoginSuccess }) => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch(`https://klickksauth.onrender.com/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
